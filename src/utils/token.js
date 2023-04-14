1444827cdddf536bbd66e006c9333cfb33cd1680
bipro <biprocuet@gmail.com>
2023-04-14 12:40:25 +0600

    lock file

--- src/utils/token.js
+++ src/utils/token.js
@@ -7,7 +7,7 @@ exports.generateToken = (userInfo)=>{
     };
 
     const token = jwt.sign(payload,process.env.TOKEN_SECRET,{
-        expiresIn:"600",
+        expiresIn:"6000000",
     })
     return token;
 }
